2e64275161df3aef200fef2a031b91c518421a13
Caio Diego <caiodsj96@gmail.com>
2023-04-03 18:47:46 -0300

    Primeiro commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Saque_Deposito {
+    class ContaBancaria {
+
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria() { }
+
+        public void NumConta(int nconta) { NumeroConta = nconta; }
+        public void DepositoInicial(string depositar) {
+            if (depositar == "s" || depositar == "S") {
+                Console.Write("Entre o valor de depósito inicial: ");
+                Saldo = double.Parse(Console.ReadLine().Replace('.', ','));
+            }
+        }
+        public override string ToString() {
+            return "Conta " + NumeroConta + ", Titular: " + Nome + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AdicionarSaldo(double deposito) {
+            Saldo += deposito;
+        }
+
+        public void RetirarSaldo(double saque) { Saldo = Saldo - saque - 5.0; }
+    }
+
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Saque_Deposito {
+    internal class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta = new ContaBancaria();
+
+            Console.Write("Entre o número da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            conta.NumConta(numeroconta);
+            Console.Write("Entre o titular da conta: ");
+            conta.Nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string resp = Console.ReadLine();
+            conta.DepositoInicial(resp);
+            Console.WriteLine("Dados da conta:\n" + conta);
+
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine().Replace('.', ','));
+            conta.AdicionarSaldo(deposito);
+            Console.WriteLine("Dados da conta atualizados:\n" + conta);
+
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine().Replace('.', ','));
+            conta.RetirarSaldo(saque);
+            Console.WriteLine("Dados da conta atualizados:\n" + conta);
+
+        }
+    }
+}
